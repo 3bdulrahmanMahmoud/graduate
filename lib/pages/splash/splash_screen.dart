e6b5dbdd0f2dafc9a0ffdc13403b956ccac8db08
Abdulrahman <2bdom27moud@gmail.com>
2024-05-31 12:35:28 +0300

    change in splash

--- lib/pages/splash/splash_screen.dart
+++ lib/pages/splash/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 import 'package:flutter/material.dart';
 import 'package:incubtor/pages/OnboardingPage/onboarding.dart';
+import 'package:incubtor/pages/Visiting/VisitingPage.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -14,36 +15,34 @@ class SplashScreen extends StatelessWidget {
       splash: SizedBox(
         height: heightQuery,
         width: WidthQuery,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Image.asset('asset/images/Group 215.png'),
-              ],
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            SizedBox(
-              height: heightQuery * .25,
-              child: Center(
-                child: Column(
-                  children: [
-                    Image.asset(
-                      'asset/images/photo_2023-11-06_22-48-31 1.png',
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    const Text(
-                      'My Incubtor',
-                      style: TextStyle(fontSize: 30),
-                    ),
-                  ],
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset('asset/images/Group 215.png'),
+                ],
+              ),
+              SizedBox(
+                height: heightQuery * .04,
+              ),
+              SizedBox(
+                width: WidthQuery*.3,
+                height: heightQuery*.3,
+                child: Image.asset(
+                  'asset/images/photo_2023-11-06_22-48-31 1.png',
+                  fit: BoxFit.scaleDown,
+                  
                 ),
               ),
-            ),
-          ],
+              
+             
+               Text(
+                'My Incubtor',
+                style: TextStyle(fontSize: 30,color: Colors.white),textAlign: TextAlign.center,
+                           ),
+            ],
+          ),
         ),
       ),
       nextScreen: OnBoarding(),
